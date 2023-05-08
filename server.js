34024bf119463bc1660ea80a956adfbb8fc2ef40
Vivek <vc1262461@gmail.com>
2023-05-09 00:57:35 +0530

    Commit

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 
-const router = jsonServer.router("data/db.json"); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
-
-app.use(function (req, res, next) {
+server.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*");
 
