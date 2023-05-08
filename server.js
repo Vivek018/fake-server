11911375f379a074a52aee7f366b6f9f0b0eca12
Vivek <vc1262461@gmail.com>
2023-05-09 01:09:05 +0530

    Added Cors

--- server.js
+++ server.js
@@ -1,33 +1,12 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 
-server.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
 const router = jsonServer.router("data/db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
